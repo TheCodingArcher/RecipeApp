d9718773d6c0cca411cb8b9a133cde983f1a0ebe
TheCodingArcher <rick_thegladiator@yahoo.com>
2019-11-25 21:03:10 +0530

    Working with Insert Annotation

--- app/src/main/java/android/the/coding/archer/recipeapp/db/RecipeStepDao.java
+++ app/src/main/java/android/the/coding/archer/recipeapp/db/RecipeStepDao.java
@@ -1,7 +1,14 @@
 package android.the.coding.archer.recipeapp.db;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.the.coding.archer.recipeapp.model.RecipeStep;
+
+import java.util.List;
 
 @Dao
 public interface RecipeStepDao {
+
+    @Insert
+    void insertAll(List<RecipeStep> steps);
 }
